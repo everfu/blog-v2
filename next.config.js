f7e43e03b5c26ab3c878e6482e9527148e6eeed7
伍拾柒 <o@efu.me>
2026-06-02 17:04:34 +0800

    chore: update dependencies and image config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,18 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   experimental: {
     optimizeCss: true,
   },
   images: {
-    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      }
-    ]
+      { protocol: 'https', hostname: 'cdn.lightxi.com' },
+      { protocol: 'https', hostname: 'wmimg.com' },
+      { protocol: 'https', hostname: 'unavatar.webp.se' },
+      { protocol: 'https', hostname: '7.isyangs.cn' },
+      { protocol: 'https', hostname: 'www.zhilu.site' },
+    ],
   },
 }
 
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig
